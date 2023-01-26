5cbd60d955e2ed3bd9472641a7a63c7cf8e26e7e
Balbaar <hjalmar.bulow@gmail.com>
2023-01-26 20:56:01 +0100

    added play pause button

--- timer/script.js
+++ timer/script.js
@@ -24,50 +24,62 @@ var hours = Math.floor(timer/3600)
 var minutes = Math.floor((timer - hours*60)/60)
 var seconds = Math.floor(timer - hours*60*60 - minutes*60)
 
+var run = false
 
 console.log(hours,minutes,seconds)
 
 setInterval(function() {
-    if((digit1.classList).length >= 3){
-        digit1.classList.remove("num-undefined", "num-0", "num-1", 
-                                "num-2", "num-3", "num-4", 
-                                "num-5", "num-6", "num-7", 
-                                "num-8", "num-9")
-    };
-    if((digit2.classList).length >= 3){
-        digit2.classList.remove("num-undefined", "num-0", "num-1", 
-                                "num-2", "num-3", "num-4", 
-                                "num-5", "num-6", "num-7", 
-                                "num-8", "num-9")
-    };
-    if((digit3.classList).length >= 3){
-        digit3.classList.remove("num-undefined", "num-0", "num-1", 
-                                "num-2", "num-3", "num-4", 
-                                "num-5", "num-6", "num-7", 
-                                "num-8", "num-9")
-    };
-    if((digit4.classList).length >= 3){
-        digit4.classList.remove("num-undefined", "num-0", "num-1", 
-                                "num-2", "num-3", "num-4", 
-                                "num-5", "num-6", "num-7", 
-                                "num-8", "num-9")
-    };
+    if(run == true) {
 
-
-    timer -= 1
+        if((digit1.classList).length >= 3){
+            digit1.classList.remove("num-undefined", "num-0", "num-1", 
+                                    "num-2", "num-3", "num-4", 
+                                    "num-5", "num-6", "num-7", 
+                                    "num-8", "num-9")
+        };
+        if((digit2.classList).length >= 3){
+            digit2.classList.remove("num-undefined", "num-0", "num-1", 
+                                    "num-2", "num-3", "num-4", 
+                                    "num-5", "num-6", "num-7", 
+                                    "num-8", "num-9")
+        };
+        if((digit3.classList).length >= 3){
+            digit3.classList.remove("num-undefined", "num-0", "num-1", 
+                                    "num-2", "num-3", "num-4", 
+                                    "num-5", "num-6", "num-7", 
+                                    "num-8", "num-9")
+        };
+        if((digit4.classList).length >= 3){
+            digit4.classList.remove("num-undefined", "num-0", "num-1", 
+                                    "num-2", "num-3", "num-4", 
+                                    "num-5", "num-6", "num-7", 
+                                    "num-8", "num-9")
+        };
     
-    hours = Math.floor(timer/3600)
-    minutes = Math.floor((timer - hours*60)/60)
-    seconds = Math.floor(timer - hours*60*60 - minutes*60)
-    console.log(hours,minutes,seconds)
-
     
-    digit1.classList.toggle(`num-${Math.floor(minutes/10)}`)
-    digit2.classList.toggle(`num-${minutes%10}`)
-    digit3.classList.toggle(`num-${Math.floor(seconds/10)}`)
-    digit4.classList.toggle(`num-${seconds%10}`)
-
-
-
-
+        timer -= 1
+        
+        hours = Math.floor(timer/3600)
+        minutes = Math.floor((timer - hours*60)/60)
+        seconds = Math.floor(timer - hours*60*60 - minutes*60)
+        console.log(hours,minutes,seconds)
+    
+        
+        digit1.classList.toggle(`num-${Math.floor(minutes/10)}`)
+        digit2.classList.toggle(`num-${minutes%10}`)
+        digit3.classList.toggle(`num-${Math.floor(seconds/10)}`)
+        digit4.classList.toggle(`num-${seconds%10}`)
+    };
 }, 1000);
+
+
+function togglePause() {
+    console.log("Play & Pause")
+    document.getElementById("playpause").classList.toggle("active")
+    if(run == false) {
+        run = true
+    }
+    else {
+        run = false
+    }
+};
